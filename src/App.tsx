2ee07e8a7f99a20327ae9cfd5511959a336f2151
cetteup <me@cetteup.com>
2022-04-05 20:05:22 +0200

    feat: Add URL join routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import JoinContainer from './components/JoinContainer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App bg-secondary bg-gradient">
+            <main>
+                <Container className="d-flex align-items-center justify-content-center vh-100 text-center">
+                    <div className="p-5 mb-2 bg-dark text-white rounded-3 w-75">
+                        <Routes>
+                            <Route path='g'>
+                                <Route path=':game/:ip::port' element={<JoinContainer />} />
+                            </Route>
+                            <Route path="*" element={<h1 className="text-white-50 display-6">Nothing to see here, double check the URL.</h1>} />
+                        </Routes>
+
+                    </div>
+                </Container>
+
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+
 );
 
 // If you want to start measuring performance in your app, pass a function
