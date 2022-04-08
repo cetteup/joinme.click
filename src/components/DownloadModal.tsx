303e01a3db697328ad6447f75c3d95fccdb41626
cetteup <me@cetteup.com>
2022-04-08 18:15:53 +0200

    feat: Add support for Battlefield 2

--- src/components/DownloadModal.tsx
+++ src/components/DownloadModal.tsx
@@ -5,17 +5,28 @@ type DownloadModalProps = {
     title: string
     protocol: string
     sourceURL: string
+    sourceProvider: string
     downloadURL: string
     downloadChecksums: {
         md5: string
         sha1: string
         sha256: string
     }
+    filename: string
     show: boolean
     onHide: () => void
 }
 
-const DownloadModal: FC<DownloadModalProps> = (props) => {
+const DownloadModal: FC<DownloadModalProps> = ({
+    title,
+    protocol,
+    sourceURL,
+    sourceProvider,
+    downloadURL,
+    downloadChecksums,
+    filename,
+    ...props
+}: DownloadModalProps) => {
     return (
         <Modal
             {...props}
@@ -24,24 +35,24 @@ const DownloadModal: FC<DownloadModalProps> = (props) => {
             centered
         >
             <Modal.Body className="bg-secondary text-white">
-                <h4> {props.title}</h4>
+                <h4> {title}</h4>
                 <p>
                     Depending on your brower, you may receive a security warning when downloading the launcher. 
-                    If you are unsure whether the download is safe, you can check the <a href={props.sourceURL} target="_blank" className="text-white" rel="noreferrer">source code on GitHub</a> and/or validate the checksum of the executable after the download. 
+                    If you are unsure whether the download is safe, you can check the <a href={sourceURL} target="_blank" className="text-white" rel="noreferrer">source code on {sourceProvider}</a> and/or validate the checksum of the executable after the download.
                 </p>
                 <p>
-                    <strong>MD5:</strong> <code className="text-white">{props.downloadChecksums.md5}</code><br />
-                    <strong>SHA1:</strong> <code className="text-white">{props.downloadChecksums.sha1}</code><br />
-                    <strong>SHA256:</strong> <code className="text-white">{props.downloadChecksums.sha256}</code>
+                    <strong>MD5:</strong> <code className="text-white">{downloadChecksums.md5}</code><br />
+                    <strong>SHA1:</strong> <code className="text-white">{downloadChecksums.sha1}</code><br />
+                    <strong>SHA256:</strong> <code className="text-white">{downloadChecksums.sha256}</code>
                 </p>
                 <p>
-                    Once downloaded and verified, right click on the <code className="text-info">{props.protocol}-url-launcher.exe</code> and then choose <q>Run as administrator</q>.
-                    This step is required in order to register it as the handler for the <code className="text-info">{props.protocol}://</code> URL protocol.
+                    Once downloaded and verified, right click on the <code className="text-info">{filename}</code> and then choose <q>Run as administrator</q>.
+                    This step is required in order to register it as the handler for the <code className="text-info">{protocol}://</code> URL protocol.
                 </p>
             </Modal.Body>
             <Modal.Footer className="bg-secondary text-white">
                 <Button variant="secondary bg-light text-dark" onClick={props.onHide}>Close</Button>
-                <Button variant="primary" href={props.downloadURL}>Download</Button>
+                <Button variant="primary" href={downloadURL}>Download</Button>
             </Modal.Footer>
         </Modal>
     );
--- src/components/JoinContainer.tsx
+++ src/components/JoinContainer.tsx
@@ -7,48 +7,73 @@ import DownloadModal from './DownloadModal';
 type GameConfig = {
     protocol: string
     label: string
+    launcher: LauncherDetails
+}
+
+type LauncherDetails = {
     sourceURL: string
-    launcherDownloadURL: string
-    launcherChecksums:  {
+    sourceProvider: string
+    downloadURL: string
+    checksums:  {
         md5: string
         sha1: string
         sha256: string
     }
+    filename: string
 }
+
+const officialLauncher: LauncherDetails = {
+    sourceURL: 'https://github.com/cetteup/joinme.click-launcher',
+    sourceProvider: 'GitHub',
+    downloadURL: 'https://github.com/cetteup/joinme.click-launcher/releases/download/v0.1.1-alpha/joinme.click-launcher-v0.1.1-alpha-windows-amd64.zip',
+    checksums: {
+        md5: '819f4340fc64f609a0c927523e7bafc6',
+        sha1: '5bb0fd8faeeaeb5208528ac60bace5c151db70c3',
+        sha256: '2ffe7cb902147dd9aec4ff959995b41466da50760a1b8155ee24e37cf91c710c'
+    },
+    filename: 'joinme.click-launcher.exe'
+};
 
 const supportedGames: Record<string, GameConfig> = {
     bf1942: {
         protocol: 'bf1942',
         label: 'Battlefield 1942',
-        sourceURL: 'https://gist.github.com/cetteup/da31a6c2e53b872d96081e859ad178a4#file-bf1942-url-launcher-ps1',
-        launcherDownloadURL: 'https://static.cetteup.com/bf1942/bf1942-url-launcher.exe',
-        launcherChecksums: {
-            md5: '1337ded869c37e0f02d4ca6028cbd618',
-            sha1: 'e138698250b6a5a8e00e99a07ee22459e0f82f90',
-            sha256: '5ca1cbeab4d2059abe84a9a051c22b36767271aac05800bbe27134338d97e9ad'
+        launcher: {
+            sourceURL: 'https://gist.github.com/cetteup/da31a6c2e53b872d96081e859ad178a4#file-bf1942-url-launcher-ps1',
+            sourceProvider: 'GitHub',
+            downloadURL: 'https://static.cetteup.com/bf1942/bf1942-url-launcher.exe',
+            checksums: {
+                md5: '1337ded869c37e0f02d4ca6028cbd618',
+                sha1: 'e138698250b6a5a8e00e99a07ee22459e0f82f90',
+                sha256: '5ca1cbeab4d2059abe84a9a051c22b36767271aac05800bbe27134338d97e9ad'
+            },
+            filename: 'bf1942-url-launcher.exe'
         }
     },
     bfvietnam: {
         protocol: 'bfv',
         label: 'Battlefield Vietnam',
-        sourceURL: 'https://gist.github.com/cetteup/c40d75c1b6d0eb32e5ebc1f329c7a5c8#file-bfv-url-launcher-ps1',
-        launcherDownloadURL: 'https://static.cetteup.com/bfvietnam/bfv-url-launcher.exe',
-        launcherChecksums: {
-            md5: 'bc26efd5f021b4a70713a9c837b71129',
-            sha1: 'd67920e55ce0aa3ede7ba1b675a4aa2cfbad050d',
-            sha256: '5be9c4a7f65a96a4bfd835da036911aebd0f82efef978fb8226434fbda807992'
+        launcher: {
+            sourceURL: 'https://gist.github.com/cetteup/c40d75c1b6d0eb32e5ebc1f329c7a5c8#file-bfv-url-launcher-ps1',
+            sourceProvider: 'GitHub',
+            downloadURL: 'https://static.cetteup.com/bfvietnam/bfv-url-launcher.exe',
+            checksums: {
+                md5: 'bc26efd5f021b4a70713a9c837b71129',
+                sha1: 'd67920e55ce0aa3ede7ba1b675a4aa2cfbad050d',
+                sha256: '5be9c4a7f65a96a4bfd835da036911aebd0f82efef978fb8226434fbda807992'
+            },
+            filename: 'bfv-url-launcher.exe'
         }
+    },
+    bf2: {
+        protocol: 'bf2',
+        label: 'Battlefield 2',
+        launcher: officialLauncher
     },
     paraworld: {
         protocol: 'paraworld',
         label: 'ParaWorld',
-        sourceURL: 'https://gist.github.com/cetteup/087675f13702dddda0a7245d2fb4cab8#file-paraworld-url-launcher-ps1',
-        launcherDownloadURL: 'https://static.cetteup.com/paraworld/paraworld-url-launcher.exe',
-        launcherChecksums: {
-            md5: '874bd81b2c3b642ee80a7ee52aa84257',
-            sha1: '4b71d8d5a5c5b7f8fabb16f095bce5be08729645',
-            sha256: '5cb2aeaf3105e1853b65ed35df3f13e3219d66ab3f62cfee4f3018b4b5622f18'
-        }
+        launcher: officialLauncher
     }
 };
 
@@ -81,9 +106,11 @@ const JoinContainer: FC = () => {
             <DownloadModal
                 title={'Download the launcher for ' + config.label}
                 protocol={config.protocol}
-                sourceURL={config.sourceURL}
-                downloadURL={config.launcherDownloadURL}
-                downloadChecksums={config.launcherChecksums}
+                sourceURL={config.launcher.sourceURL}
+                sourceProvider={config.launcher.sourceProvider}
+                downloadURL={config.launcher.downloadURL}
+                downloadChecksums={config.launcher.checksums}
+                filename={config.launcher.filename}
                 show={modalShow}
                 onHide={() => setModalShow(false)}
             />
