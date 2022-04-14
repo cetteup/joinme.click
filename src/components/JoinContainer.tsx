f457d6c1a53ff7ba948c5d7241d5ab6d5f91f35f
cetteup <me@cetteup.com>
2022-04-14 16:35:34 +0200

    feat: Add support for ET: Legacy

--- src/components/JoinContainer.tsx
+++ src/components/JoinContainer.tsx
@@ -26,26 +26,42 @@ const JoinContainer: FC = () => {
         <>
             <h1 className="display-6">You have been invited to join {ip}:{port}, a <em>{config.label}</em> server</h1>
             <div>
-                <JoinBtn className="mt-3 mx-3" protocol={config.protocol} ip={ip} port={port} />
-                <Button className="mt-3 mx-3" variant="outline-secondary" size="lg" onClick={() => setModalShow(true)}>Download launcher</Button>
+                <JoinBtn className="mt-3 mx-3" protocol={config.protocol} ip={ip} port={port}/>
+                {
+                    config.requiresLauncher &&
+                    <Button className="mt-3 mx-3" variant="outline-secondary" size="lg" onClick={() => setModalShow(true)}>Download launcher</Button>
+                }
             </div>
-            <div className="mt-3">
-                <p className="text-white-50"><small>{config.label} requires launcher version {config.minLauncherVersion} or later.
-                    You can determine your launcher version by looking at the details tab of the {config.launcher.filename} file properties.</small></p>
-            </div>
+            {
+                (config.requiresLauncher && config.launcher || config.hint) &&
+                <div className="mt-3">
+                    <p className="text-white-50">
+                        {
+                            config.requiresLauncher && config.launcher &&
+                            <small>{config.label} requires launcher version {config.minLauncherVersion} or later.
+                                You can determine your launcher version by looking at the details tab of the {config.launcher.filename} file properties.</small>
+                        }
+                        {
+                            config.hint
+                        }
+                    </p>
+                </div>
+            }
 
-
-            <DownloadModal
-                title={'Download the launcher'}
-                protocol={config.protocol}
-                sourceURL={config.launcher.sourceURL}
-                sourceProvider={config.launcher.sourceProvider}
-                downloadURL={config.launcher.downloadURL}
-                downloadChecksums={config.launcher.checksums}
-                filename={config.launcher.filename}
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
+            {
+                config.requiresLauncher && config.launcher &&
+                <DownloadModal
+                    title={'Download the launcher'}
+                    protocol={config.protocol}
+                    sourceURL={config.launcher.sourceURL}
+                    sourceProvider={config.launcher.sourceProvider}
+                    downloadURL={config.launcher.downloadURL}
+                    downloadChecksums={config.launcher.checksums}
+                    filename={config.launcher.filename}
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                />
+            }
         </>
     );
 };
--- src/games/titles.tsx
+++ src/games/titles.tsx
@@ -1,94 +1,117 @@
+import React from 'react';
 import { LauncherDetails, officialLauncher } from './launchers';
 
 export type GameConfig = {
     protocol: string
     label: string
-    launcher: LauncherDetails
-    minLauncherVersion: string
+    requiresLauncher: boolean
+    launcher?: LauncherDetails
+    minLauncherVersion?: string
+    hint?: JSX.Element
 }
 
 export const supportedGames: Record<string, GameConfig> = {
     bf1942: {
         protocol: 'bf1942',
         label: 'Battlefield 1942',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.2-alpha'
     },
     bfvietnam: {
         protocol: 'bfvietnam',
         label: 'Battlefield Vietnam',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.2-alpha'
     },
     bf2: {
         protocol: 'bf2',
         label: 'Battlefield 2',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.1-alpha'
     },
     bf2sf: {
         protocol: 'bf2sf',
         label: 'Battlefield 2: Special Forces',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     cod: {
         protocol: 'cod',
         label: 'Call of Duty',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     coduo: {
         protocol: 'coduo',
         label: 'Call of Duty: United Offensive',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     cod2: {
         protocol: 'cod2',
         label: 'Call of Duty 2',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     cod4: {
         protocol: 'cod4',
         label: 'Call of Duty 4: Modern Warfare',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     codwaw: {
         protocol: 'codwaw',
         label: 'Call of Duty: World at War',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
+    },
+    et: {
+        protocol: 'et',
+        label: 'ET: LEGACY',
+        requiresLauncher: false,
+        hint: (<small>ET: Legacy does not require a launcher. You only need to have the game installed, which you can get at <a href={'https://www.etlegacy.com/download'} target='_blank' className="text-white" rel="noreferrer">etlegacy.com/download</a></small>)
     },
     fearsec2: {
         protocol: 'fearsec2',
         label: 'F.E.A.R. Combat (SEC2)',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     paraworld: {
         protocol: 'paraworld',
         label: 'ParaWorld',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.2-alpha'
     },
     swat4: {
         protocol: 'swat4',
         label: 'SWAT 4',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     swat4x: {
         protocol: 'swat4x',
         label: 'SWAT 4: The Stetchkov Syndicate',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     },
     vietcong: {
         protocol: 'vietcong',
         label: 'Vietcong',
+        requiresLauncher: true,
         launcher: officialLauncher,
         minLauncherVersion: 'v0.1.3-alpha'
     }
