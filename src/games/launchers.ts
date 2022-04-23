b08ca832a2cb9ccf2f6378dc33c99b5406209cc6
cetteup <me@cetteup.com>
2022-04-23 21:57:55 +0200

    chore: Bump launcher version to v0.1.7-alpha

--- src/games/launchers.ts
+++ src/games/launchers.ts
@@ -14,12 +14,12 @@ export type LauncherDetails = {
 export const officialLauncher: LauncherDetails = {
     sourceURL: 'https://github.com/cetteup/joinme.click-launcher',
     sourceProvider: 'GitHub',
-    currentVersion: 'v0.1.6-alpha',
-    downloadURL: 'https://github.com/cetteup/joinme.click-launcher/releases/download/v0.1.6-alpha/joinme.click-launcher-v0.1.6-alpha-windows-amd64.zip',
+    currentVersion: 'v0.1.7-alpha',
+    downloadURL: 'https://github.com/cetteup/joinme.click-launcher/releases/download/v0.1.7-alpha/joinme.click-launcher-v0.1.7-alpha-windows-amd64.zip',
     checksums: {
-        md5: 'f9551bc5627b1f8c3e0f29ae199c4bd0',
-        sha1: '503bf47b1b0045f39ed39880b546f7a1d16fa384',
-        sha256: 'eb4b492835c8953b9eae5a55f0f7ac957a5d9c770bebea9e12ce3bbca4e644a1'
+        md5: '59e82cc7358b9a5468b69c584d22e58d',
+        sha1: 'a845e8960c58353bb85711330ab6f46adba8b3d6',
+        sha256: 'a715acfffcb46cdc535ce0f753513a01d0b3aaa0be2335e669f6b928b5376dc0'
     },
     filename: 'joinme.click-launcher.exe'
 };
