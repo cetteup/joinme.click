be8d0c7b695150cf1969afb24c851ea419cf8920
cetteup <me@cetteup.com>
2022-07-27 18:59:14 +0200

    feat: Add Discord link to footer

--- src/elements/Footer.tsx
+++ src/elements/Footer.tsx
@@ -3,10 +3,14 @@ import IconLink from '../components/atoms/IconLink';
 
 const Footer: FC = () => {
     return (
-        <footer className='mt-3 fs-4'>
-            <IconLink icon={'bi-github'} href={'https://github.com/cetteup/joinme.click'} />
-            <IconLink icon={'bi-twitter'} href={'https://twitter.com/cetteup'} />
-            <IconLink icon={'bi-send-fill'} href={'mailto:me@cetteup.com'} />
+        <footer className='mt-4'>
+            <p className={'fs-5'}><small>Need help? Join <a href={'https://discord.gg/wwsuMk9g4E'}>our Discord.</a></small></p>
+            <div className={'fs-4'}>
+                <IconLink icon={'bi-discord'} href={'https://discord.gg/wwsuMk9g4E'} />
+                <IconLink icon={'bi-github'} href={'https://github.com/cetteup/joinme.click'} />
+                <IconLink icon={'bi-twitter'} href={'https://twitter.com/cetteup'} />
+                <IconLink icon={'bi-send-fill'} href={'mailto:me@cetteup.com'} />
+            </div>
         </footer>
     );
 };
