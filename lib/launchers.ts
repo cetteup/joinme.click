65e275b149cb30257144a61e2f46705349fb9363
cetteup <me@cetteup.com>
2023-02-10 22:20:45 +0100

    chore: Bump launcher version to v0.2.0

--- lib/launchers.ts
+++ lib/launchers.ts
@@ -14,12 +14,12 @@ export type LauncherDetails = {
 export const officialLauncher: LauncherDetails = {
     sourceURL: 'https://github.com/cetteup/joinme.click-launcher',
     sourceProvider: 'GitHub',
-    currentVersion: 'v0.1.12',
-    downloadURL: 'https://github.com/cetteup/joinme.click-launcher/releases/download/v0.1.12/joinme.click-launcher-v0.1.12-windows-amd64.zip',
+    currentVersion: 'v0.2.0',
+    downloadURL: 'https://github.com/cetteup/joinme.click-launcher/releases/download/v0.2.0/joinme.click-launcher-v0.2.0-windows-amd64.zip',
     checksums: {
-        md5: '92ba5790a60f01c31a77f911fb694e45',
-        sha1: '2a9b5d8b12f0311fb6ae2f72c6dd3ce43f7f1f34',
-        sha256: '772823d67e91f14bf4afbaa9c40d9410ff7b7a32ccc5f3d052da93773896a455'
+        md5: '31236db48743ab7a4fcd1133b8803960',
+        sha1: 'c953b2aa1857b954575600884a67a936fc8487f1',
+        sha256: 'b85b3e08df672afef3de80901c3d3bf067718463f8aa52abdd10ee7de379fe09'
     },
     filename: 'joinme.click-launcher.exe'
 };
