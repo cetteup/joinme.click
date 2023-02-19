f2fd05c6a07991ff1356d4841fd402b0392b6f64
cetteup <me@cetteup.com>
2023-02-19 20:48:52 +0100

    feat: Link to launcher instead of website repo in footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -43,7 +43,7 @@ const Footer: FC = () => {
             <p className={'fs-5 text-white-50'}><small>Need help? Join <a href={'https://discord.gg/wwsuMk9g4E'} className="text-white-50">our Discord</a></small></p>
             <div className={'fs-4'}>
                 <IconLink icon={'bi-discord'} href={'https://discord.gg/wwsuMk9g4E'} title={'Join our Discord'} />
-                <IconLink icon={'bi-github'} href={'https://github.com/cetteup/joinme.click'} title={'View source code on GitHub'} />
+                <IconLink icon={'bi-github'} href={'https://github.com/cetteup/joinme.click-launcher'} title={'View source code on GitHub'} />
                 <IconLink icon={'bi-cloud-download'} href={'/download'} title={'Download the launcher'} />
                 <IconLink icon={'bi-twitter'} href={'https://twitter.com/cetteup'} title={'Reach out on Twitter'} />
                 <IconLink icon={'bi-send-fill'} href={'mailto:me@cetteup.com'} title={'Reach out via email'} />
