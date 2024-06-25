1f7b625470e0416ed5c0ff117529eab98724ccb4
cetteup <me@cetteup.com>
2024-06-25 18:57:22 +0200

    chore: Update Discord invite link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -42,7 +42,7 @@ const Footer: FC = () => {
         <footer className='mt-4'>
             <p className={'fs-5 text-white-50'}><small>Need help? Join <a href={'https://discord.gg/wwsuMk9g4E'} className="text-white-50">our Discord</a></small></p>
             <div className={'fs-4'}>
-                <IconLink icon={'bi-discord'} href={'https://discord.gg/wwsuMk9g4E'} title={'Join our Discord'} />
+                <IconLink icon={'bi-discord'} href={'https://discord.gg/66u9udBF57'} title={'Join our Discord'} />
                 <IconLink icon={'bi-github'} href={'https://github.com/cetteup/joinme.click-launcher'} title={'View source code on GitHub'} />
                 <IconLink icon={'bi-cloud-download'} href={'/download'} title={'Download the launcher'} />
                 <IconLink icon={'bi-twitter'} href={'https://twitter.com/cetteup'} title={'Reach out on Twitter'} />
